7fe10499f6f27a8fc4f63ab0925c632e586cb7e9
luxebo <myyuen@uci.edu>
2026-06-26 19:33:19 -0700

    fixes to issues on style

--- scripts/theme_changer.js
+++ scripts/theme_changer.js
@@ -1,9 +1,9 @@
-if (window.location.href.includes("hackers"))
-    document.body.setAttribute("data-md-color-scheme", "hackers");
+if (window.location.href.includes("hacker"))
+    document.body.setAttribute("data-md-color-scheme", "hacker");
 else if (window.location.href.includes("NeutralKilling"))
     document.body.setAttribute("data-md-color-scheme", "nk");
-else if (window.location.href.includes("feds"))
-    document.body.setAttribute("data-md-color-scheme", "feds");
+else if (window.location.href.includes("fed"))
+    document.body.setAttribute("data-md-color-scheme", "fed");
 else if (window.location.href.includes("w3c"))
     document.body.setAttribute("data-md-color-scheme", "w3c");
 else if (window.location.href.includes("paragon"))
