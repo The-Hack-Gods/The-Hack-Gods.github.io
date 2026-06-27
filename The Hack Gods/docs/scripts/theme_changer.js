30353e793d0735a6a59cb5426d96f7ff12e3e671
luxebo <myyuen@uci.edu>
2026-06-26 21:05:25 -0700

    fix more stuff

--- The Hack Gods/docs/scripts/theme_changer.js
+++ The Hack Gods/docs/scripts/theme_changer.js
@@ -1,10 +1,10 @@
 if (window.location.href.includes("hacker"))
     document.body.setAttribute("data-md-color-scheme", "hacker");
-else if (window.location.href.includes("NeutralKilling"))
-    document.body.setAttribute("data-md-color-scheme", "nk");
 else if (window.location.href.includes("fed"))
     document.body.setAttribute("data-md-color-scheme", "fed");
 else if (window.location.href.includes("w3c"))
     document.body.setAttribute("data-md-color-scheme", "w3c");
 else if (window.location.href.includes("paragon"))
     document.body.setAttribute("data-md-color-scheme", "paragon");
+else if (window.location.href.includes("NeutralKilling"))
+    document.body.setAttribute("data-md-color-scheme", "nk");
